a41dc3f9a95c0113982275a9f61763f8924369c8
Divyansh Kushwaha <69595691+divyansh-dxn@users.noreply.github.com>
2021-08-14 20:48:08 +0530

    Update ExtensionFunctions.kt

--- ExtensionFunctions.kt
+++ ExtensionFunctions.kt
@@ -1,1 +1,31 @@
+// Save wallpaper to external storage on devices running android Q and higher, accessible to other apps
+@RequiresApi(Build.VERSION_CODES.Q)
+suspend fun Context.saveWallpaperQ(wallpaper: Wallpaper): Result<Boolean?> {
+    return withContext(Dispatchers.IO) {
+        val filename = "${wallpaper.name}.jpg"
+        val filesDir = Environment.DIRECTORY_PICTURES + "/WallpaperX"
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/png")
+            put(MediaStore.MediaColumns.DATE_ADDED, System.currentTimeMillis() / 1000)
+            put(MediaStore.MediaColumns.DATE_TAKEN, System.currentTimeMillis())
+            put(MediaStore.MediaColumns.RELATIVE_PATH, filesDir)
 
+        }
+        val bmp = getBitmap(wallpaper.imageUri)
+        val imageUri: Uri? =
+            this@saveWallpaperQ.contentResolver.insert(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues
+            )
+        val result = kotlin.runCatching {
+            val fos: OutputStream? = imageUri?.let { contentResolver.openOutputStream(it) }
+            fos?.use {
+                bmp.compress(Bitmap.CompressFormat.JPEG, 100, it)
+            }
+        }.onFailure {
+            Log.e("ApplicationError/Extensions", "saveWallpaperQ: $it")
+        }
+        result
+    }
+}
